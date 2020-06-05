ad4a5eccf199cef268b8d1172cb35d47a4716c66
mertoz41 <mertozkaynak@windowslive.com>
2020-06-05 10:37:31 -0400

    first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+    fetchDogs();
+});
+
+function fetchDogs(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => { json.message.forEach(element => { 
+        let imagesDiv = document.getElementById('dog-image-container');
+        let img = document.createElement('img');
+
+        img.src = element;
+        imagesDiv.appendChild(img);
+        
+        
+        })})
+}
